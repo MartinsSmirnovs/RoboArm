6d2c806ae7868a0339fab7bf34c3408af65c8c4c
Onii-Chaan <martinnc@inbox.lv>
2020-06-18 20:13:57 +0300

    Added motor distortion

--- Arm project/src/main.cpp
+++ Arm project/src/main.cpp
@@ -11,16 +11,17 @@ Motor bicep(571, 8, 7, 1);
 
 DcMotor shoulder(5, 6, 50);
 
-bool autoMode = false;
-unsigned long oldTimeInt = 0;
-bool grabState = false;
+bool autoMode = false;        //enables or disables auto mode
+unsigned long oldTimeInt = 0; //follows time
+bool grabState = false;       //finger servo grab toggle
 
 int genDelay = 5000;
 int smallDelay = 1000;
 
-byte part = 0;
-byte inPart = 0;
+byte part = 0;   //delay between big movements
+byte inPart = 0; //delay between small movements
 int values[3][4] = {
+    //arm states values
     {209, 122, 90, 139}, //down take
     {147, 170, 90, 162}, //up
     {238, 118, 90, 139}  //up put
@@ -29,70 +30,68 @@ int values[3][4] = {
 void setup()
 {
   Serial.begin(9600);
-  fingerServo.attach(9);
+  fingerServo.attach(9); //start servos
   elbowServo.attach(10);
   wristServo.attach(11);
   vertServo.attach(12);
 
-  fingerServo.write(110);
+  fingerServo.write(110); //give servos starting position
   elbowServo.write(135);
   wristServo.write(90);
   vertServo.write(90);
 
-  bicep.setup();
+  bicep.setup(); //setup other motors
   shoulder.setup();
 }
 
 void loop()
 {
-  bt.loop();
+  bt.loop(); //main loops for different objects
   bicep.loop();
   shoulder.loop();
-  checkBt();
 
-  if (autoMode)
+  checkBt(); //checks for incoming bluetooth data and proceeds it
+
+  if (autoMode) //if user have chose automode then loop it
   {
     doAuto();
   }
 }
 
-
 void checkBt()
 {
   if (bt.getNewData())
   {
-    switch (bt.getType())
+    if (!autoMode && bt.getType() >= 1 && bt.getType() <= 6) //if there is automode then user can't manual set motor values
     {
-    case 1:
-      if (!autoMode)
+      switch (bt.getType())
+      {
+      case 1:
         fingerServo.write(bt.getValue());
-      break;
-    case 2:
-      if (!autoMode)
+        break;
+      case 2:
         elbowServo.write(bt.getValue());
-      break;
-    case 3:
-      if (!autoMode)
+        break;
+      case 3:
         wristServo.write(bt.getValue());
-      break;
-    case 4:
-      if (!autoMode)
+        break;
+      case 4:
         vertServo.write(bt.getValue());
-      break;
-    case 5:
-      if (!autoMode)
+        break;
+      case 5:
         bicep.setPosition(bt.getValue());
-      break;
-    case 6:
-      if (!autoMode)
+        break;
+      case 6:
         shoulder.setPosition(bt.getValue());
-      break;
-    case 7:
-      autoMode = !autoMode;
-      grabState = false;
-      break;
-    default:
-      break;
+        break;
+      default:
+        break;
+      }
+    }
+    else if (bt.getType() == 7) //if datatype is 7 then toggle auto mode
+    {
+      autoMode = !autoMode; //toggles auto mode
+      grabState = false;    //resets grab to default open state
     }
   }
 }
--- Arm project/src/motors.cpp
+++ Arm project/src/motors.cpp
@@ -1,46 +1,46 @@
 #include "main.h"
 
-void doAuto()
+void doAuto() //automatic movement function
 {
-  switch (part)
+  switch (part) //switches between different arm states
   {
   case 0:
-    move(0);
+    move(0); // goes to default at the start
     break;
   case 1:
-    move(1);
+    move(1); //goes to pick up from down state
     break;
   case 2:
-    grab();
-    countPart();
+    grab();      //grabs
+    countPart(); //increments movement count
     break;
   case 3:
-    move(0);
+    move(0); //pulls up arm to default position
     break;
   case 4:
-    turn(true);
-    countPart();
+    turn(true);  //turns clockwise
+    countPart(); //counts parts
     break;
   case 5:
-    move(2);
+    move(2); //goes to put on top
     break;
   case 6:
-    grab();
-    countPart();
+    grab();      //release grab
+    countPart(); //counts parts
     break;
   case 7:
-    move(0);
+    move(0); //goes to default position
     break;
   case 8:
-    turn(false);
-    countPart();
+    turn(false); //turns anticlockwise
+    countPart(); //counts moevement
     break;
   default:
     break;
   }
 }
 
-void move(byte movePart)
+void move(byte movePart) //moves to values coordinates depending on movePart variable
 {
   if (millis() - oldTimeInt >= smallDelay)
   {
@@ -70,10 +70,10 @@ void move(byte movePart)
   }
 }
 
-void countPart()
+void countPart() //counts part
 {
   part++;
-  if (part == 8)
+  if (part == 9)
     part = 0;
 }
 
@@ -82,20 +82,16 @@ void turn(bool side)
   //side == true left
   //side == false right
   if (side)
-  {
     shoulder.setPosition(60);
-  }
   else
-  {
     shoulder.setPosition(0);
-  }
 }
 
 void grab()
 {
-  if (grabState) //grab
-    fingerServo.write(98);
-  else //release
-    fingerServo.write(162);
-  grabState = !grabState;
+  if (grabState)
+    fingerServo.write(98); //grab
+  else
+    fingerServo.write(162); //release
+  grabState = !grabState;   //toggles grab state variable
 }
